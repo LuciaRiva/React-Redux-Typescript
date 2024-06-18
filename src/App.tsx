b3f1c2a9705a820f04aba20fcef1276617bbbcca
LuciaRiva <luciariva.webdeveloper@gmail.com>
2024-06-18 20:57:30 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Container, Form, Button } from 'react-bootstrap';
+
+const App: React.FC = () => {
+  return (
+    <Container className="mt-5">
+      <h2>Login</h2>
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword" className="mb-4">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
+  );
+}
+
+export default App;
