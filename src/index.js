110031fa3e56e314d641d425097989470de53706
Lucia Riva <luciariva08@hotmail.com>
2024-08-08 09:05:48 -0300

    update

--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import App from './App';
-import store from './store';
-import './index.css';
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { Provider } from "react-redux";
+// import App from './App';
+// import store from './store';
+// import './index.css';
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   </React.StrictMode>
+// );
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
-);
+
+// @ts-check
+
+
+/**
+ * Nombre de usuario
+ * @type {string}
+ */
+let userName= "Lucia"
+
+/**
+ * Edad del usuario
+ *  @type {number}
+ */
+
+let userAge= 28
+
+/**
+ * Lista de edades de usuarios
+ *  @type {Array<Number>}
+ */
+
+const usersAge= [20,23,24,34]
